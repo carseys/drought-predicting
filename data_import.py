fba368d450e8a167177adee31d19b200907762b3
Simone Carsey <72096779+carseys@users.noreply.github.com>
2025-03-04 14:30:53 +0100

    data processing py class

---
+++ data_import.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+import dask.dataframe as dd
+import numpy as np
+import glob
+from tqdm import tqdm
+
+
+class OregonImport:
+    
+    def __init__(self):
+        self.import_data_dask_flag = False
+        self.oregon_only_flag = False        
+
+    def _import_data_dask(self)-> None:
+        """
+        This function imports raw data from directory using dask and returns a dictionary of dask DataFrames.
+
+        Parameters
+        ----------
+        None.
+
+        Returns
+        -------
+        None.
+        """
+        input_dir = os.path.abspath("data\\*\\*")
+
+        data_paths = glob.glob(input_dir, recursive=True)
+
+        self.raw_data_dict = {}
+
+        for data in tqdm(data_paths, desc="file import"):
+            name = os.path.basename(data).split('.')[0]
+            self.raw_data_dict[name] = dd.read_csv(data)
+        self.import_data_dask_flag = True
+
+        return None
+
+    def _oregon_only(self):
+        """
+        This function trims input data to only contain data regarding Oregon counties.
+
+        Parameters
+        ----------
+        None
+        """
+        assert self.import_data_dask_flag, "Please run _import_data_dask before _oregon_only."
+        self.oregon_data_dict = {}
+        for keyval in tqdm(self.data_dict.keys(), desc='table management'):
+            table = self.data_dict[keyval]
+            table = table.loc[(table['fips']>41000) & (table['fips']<42000)].copy()
+            table = table.compute()
+            self.oregon_data_dict[keyval] = table
+        
+        self.oregon_only_flag = True
+        return None
+    
+    def oregon_data_runner(self):
+        """
+        This function runs the other functions of the class in the correct order.
+        """
+
+        self._import_data_dask()
+        oregon_data_dict = self._oregon_only()
+
+        return oregon_data_dict
